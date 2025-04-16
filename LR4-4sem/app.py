6f8c4deaeb5c1bcf0a49bc1fd95fc1addc0d98c0
Stepanova-Anna <159694466+Stepanova-Anna@users.noreply.github.com>
2025-04-16 10:50:14 +0300

    Add files via upload

---
+++ LR4-4sem/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from controlers import CurrencyController
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+controller = CurrencyController()
+
+@app.route('/')
+def index():
+    return controller.get_rates_view()
+
+if __name__ == '__main__':
+    app.run(debug=True)
